b30c11e3a8ec1e362757699bf0de6f403eefbb6f
Valerio <valerio.taralli2001@gmail.com>
2023-07-16 18:00:06 +0200

    Piccola\ Modifica\ HeapSort

--- Projects/Raffinamento/src/ordinamento.hpp
+++ Projects/Raffinamento/src/ordinamento.hpp
@@ -58,12 +58,22 @@ namespace LibreriaOrdinamento {
 
   inline bool operator<(const Decrescente &oggetto1, const Decrescente &oggetto2)
   {
-    return (oggetto1.valore > oggetto2.valore);
+    return oggetto1.valore > oggetto2.valore;
+  }
+
+  inline bool operator<=(const Decrescente &oggetto1, const Decrescente &oggetto2)
+  {
+    return (oggetto1.valore > oggetto2.valore)||(oggetto1.valore == oggetto2.valore);
   }
 
   inline bool operator>(const Decrescente &oggetto1, const Decrescente &oggetto2)
   {
-    return (oggetto1.valore < oggetto2.valore);
+    return oggetto1.valore < oggetto2.valore;
+  }
+
+  inline bool operator>=(const Decrescente &oggetto1, const Decrescente &oggetto2)
+  {
+    return (oggetto1.valore < oggetto2.valore)||(oggetto1.valore == oggetto2.valore);
   }
 
 
@@ -74,7 +84,7 @@ namespace LibreriaOrdinamento {
   template<typename T> vector<T> HeapSort(const vector<T>& v)
   {
 
-    unsigned int g = 0, f = 0; //Indici del nodo del figlio e del genitore
+    unsigned int g = 0, f = 0, n=v.size(); //Indici del nodo del figli, del genitore e dimensione del vettore da ordinare
     vector<T> mucchioBin = v;
     T bicerino; //Variabile per memorizzare temporaneamente l'elemento da scambiare; il nome è l'adattamento del piemontese «bicerin» che letteralmente significa «bicchierino»
 
@@ -119,6 +129,82 @@ namespace LibreriaOrdinamento {
 
     }
 
+
+    //Questo ciclo «for» invece riordina la lista scambiando inizialmente il primo elemento con l'ultimo, successivamente riposizione il nuovo primo elemento
+    //(coincidente con l'ultimo di prima) in modo tale che l'albero binario rimanente, escluso l'ultimo nodo, soddisfi le caratteristiche di un mucchio binario
+    //descritte nel precedente ciclo; l'algoritmo viene ripetuto escludendo di volta in volta il primo elemento scambiato finché non finiscono i nodi del mucchio binario originale.
+
+
+    for(unsigned int k=0; k<n; k++)
+    {
+
+      //Questa parte scambia il primo con il (n-k-1)-esimo elemento con «n» coincidente con la dimensione del vettore in ingresso «v».
+      bicerino=mucchioBin[n-k-1];
+      mucchioBin[n-k-1]=mucchioBin[0];
+      mucchioBin[0]=bicerino;
+
+      g=0; //Indice del nodo genitore in cima all'albero binario
+      f=2*g+1; //Indice del primo figlio del geniotre g
+      bicerino=mucchioBin[0];
+
+      //Questa condizione verifica che il nodo genitore ha almeno un nodo figlio contenuto nel parziale mucchio binario
+      //Infatti la condizione «f<(n-k-1)» esclude il nodo scambiato all'inizio posizionato all'indice n-k-1
+      while(f<(n-k-1))
+      {
+
+        if((f+1)<(n-k-1)) //Controllo che esista anche il secondo figlio
+        {
+            if(bicerino<mucchioBin[f] && bicerino<mucchioBin[f+1]) //Controllo che sia il primo figlio corrente che secondo siano maggiori dell'iniziale valore
+            {
+                if(mucchioBin[f]>=mucchioBin[f+1]) //Verifica che il primo figlio corrente sia maggiore del secondo
+                {
+                    mucchioBin[g]=mucchioBin[f]; //Memorizzazione del valore del primo figlio corrente al genitore
+                    g=f; //Si pone il primo figlio corrente come nuovo genitore
+                }
+                else
+                {
+                    mucchioBin[g]=mucchioBin[f+1];
+                    g=f+1;
+                }
+            }
+            else if(bicerino<mucchioBin[f]) //Se il valore iniziale non è minore di entrambi i figli potrebbe comunque esserlo singolarmente e i seguenti «else if» verificano tale situazione
+            {
+                mucchioBin[g]=mucchioBin[f];
+                g=f;
+            }
+            else if(bicerino<mucchioBin[f+1])
+            {
+                mucchioBin[g]=mucchioBin[f+1];
+                g=f+1;
+            }
+            else break; //Si ferma il ciclo perché ambo i figli correnti sono minori dell'iniziale valore
+        }
+        else if(bicerino<mucchioBin[f]) //Controllo che il primo [unico] figlio sia maggiore dell'iniziale valore
+        {
+            mucchioBin[g]=mucchioBin[f];
+            g=f;
+        }
+        else break; //Si ferma il ciclo perché il primo [unico] figlio corrente è minore dell'iniziale valore
+
+        f=2*g+1; //Calcolo del prossimo primo figlio corrente
+
+      }
+
+      //Se l'indice del nodo genitore corrente differisce da quello dell'iniziale nodo genitore (ovvero g=0), allora v'è stato
+      //almeno uno scambio per cui bisogna memorizzare in fondo alla catena degli scambi il valore del nodo iniziale
+      if(g!=0) mucchioBin[g]=bicerino;
+
+    }
+
+
+    return mucchioBin;
+
+  }
+
+}
+
+#endif // __ORDINAMENTO_H
+
 /*
       //Precedente versione meno efficiente perché effettuava piú operazioni di scambio di quanto fosse necessario,
       //aumentando il costo computazionale per alti valori di «n», in particolare all'interno della struttura «if»
@@ -154,75 +240,70 @@ namespace LibreriaOrdinamento {
     }
 */
 
-    //Questo ciclo «for» invece riordina la lista scambiando inizialmente il primo elemento con l'ultimo, successivamente riposizione il nuovo primo elemento
-    //(coincidente con l'ultimo di prima) in modo tale che l'albero binario rimanente, escluso l'ultimo nodo, soddisfi le caratteristiche di un mucchio binario
-    //descritte nel precedente ciclo; l'algoritmo viene ripetuto escludendo di volta in volta il primo elemento scambiato finché non finiscono i nodi del mucchio binario originale.
+/*
 
-    for(unsigned int k=0; k<mucchioBin.size(); k++)
+  //Precedente versione meno efficiente perché effettuava piú operazioni di scambio di quanto fosse necessario,
+  //aumentando il costo computazionale per alti valori di «n», in particolare all'interno della struttura «if»
+
+for(unsigned int k=0; k<mucchioBin.size(); k++)
+{
+
+  //Questa parte scambia il primo con il (n-k-1)-esimo elemento con «n» coincidente con la dimensione del vettore in ingresso «v».
+  bicerino=mucchioBin[mucchioBin.size()-k-1];
+  mucchioBin[mucchioBin.size()-k-1]=mucchioBin[0];
+  mucchioBin[0]=bicerino;
+
+  g=0; //Indice del nodo genitore in cima all'albero binario
+
+  while(g!=v.size())
+  {
+
+    f=2*g+1; //Indice del primo figlio del geniotre g
+
+    //Controlla che l'etichetta del nodo genitore sia minore del suo primo figlio; inoltre si verifica che il primo figlio
+    //esista effettivamente tramite la condizione «f<(n-k-1)», in cui n-k-1 esclude il nodo scambiato all'inizio.
+    if(mucchioBin[g]<mucchioBin[f] && f<(v.size()-k-1))
     {
-
-      //Questa parte scambia il primo con il (n-k-1)-esimo elemento con «n» coincidente con la dimensione del vettore in ingresso «v».
-      bicerino=mucchioBin[mucchioBin.size()-k-1];
-      mucchioBin[mucchioBin.size()-k-1]=mucchioBin[0];
-      mucchioBin[0]=bicerino;
-
-      g=0; //Indice del nodo genitore in cima all'albero binario
-
-      while(g!=v.size())
-      {
-
-        f=2*g+1; //Indice del primo figlio del geniotre g
-
-        //Controlla che l'etichetta del nodo genitore sia minore del suo primo figlio; inoltre si verifica che il primo figlio
-        //esista effettivamente tramite la condizione «f<(n-k-1)», in cui n-k-1 esclude il nodo scambiato all'inizio.
-        if(mucchioBin[g]<mucchioBin[f] && f<(v.size()-k-1))
+        //Si controlla l'etichetta del secondo figlio, verificandone pure l'esistenza sempre escludendo il nodo scambiato all'inizio.
+        if(mucchioBin[g]<mucchioBin[f+1] && (f+1)<(v.size()-k-1))
         {
-            //Si controlla l'etichetta del secondo figlio, verificandone pure l'esistenza sempre escludendo il nodo scambiato all'inizio.
-            if(mucchioBin[g]<mucchioBin[f+1] && (f+1)<(v.size()-k-1))
-            {
-                //Se pure il secondo figlio è minore del genitore allora si sceglie il figlio con l'etichetta maggiore e lo si scambia con il genitore.
-                if(mucchioBin[f]>mucchioBin[f+1])
-                {
-                     bicerino = mucchioBin[f];
-                     mucchioBin[f] = mucchioBin[g];
-                     mucchioBin[g] = bicerino;
-                     g=f;
-                }
-                else
-                {
-                     bicerino = mucchioBin[f+1];
-                     mucchioBin[f+1] = mucchioBin[g];
-                     mucchioBin[g] = bicerino;
-                     g=f+1;
-                }
-            }
-            //Altrimenti se solo il primo figlio è maggiore con il genitore si scambiano questi due senza indugio.
-            else
+            //Se pure il secondo figlio è minore del genitore allora si sceglie il figlio con l'etichetta maggiore e lo si scambia con il genitore.
+            if(mucchioBin[f]>mucchioBin[f+1])
             {
                  bicerino = mucchioBin[f];
                  mucchioBin[f] = mucchioBin[g];
                  mucchioBin[g] = bicerino;
                  g=f;
             }
+            else
+            {
+                 bicerino = mucchioBin[f+1];
+                 mucchioBin[f+1] = mucchioBin[g];
+                 mucchioBin[g] = bicerino;
+                 g=f+1;
+            }
         }
-        //In caso il primo figlio non abbia etichetta maggiore del genitore si ripetono le stesse condizioni al secondo figlio
-        else if(mucchioBin[g]<mucchioBin[f+1] && (f+1)<(v.size()-k-1))
+        //Altrimenti se solo il primo figlio è maggiore con il genitore si scambiano questi due senza indugio.
+        else
         {
-             bicerino = mucchioBin[f+1];
-             mucchioBin[f+1] = mucchioBin[g];
+             bicerino = mucchioBin[f];
+             mucchioBin[f] = mucchioBin[g];
              mucchioBin[g] = bicerino;
-             g=f+1;
+             g=f;
         }
-        //Se ambo le condizioni non sono verificate il nodo ha un etichetta maggiore di ambo i suoi [eventuali] nodi figli, condizione che si verifica solo se l'algoritmo è terminato.
-        else g=v.size();
-
-      }
     }
-
-    return mucchioBin;
+    //In caso il primo figlio non abbia etichetta maggiore del genitore si ripetono le stesse condizioni al secondo figlio
+    else if(mucchioBin[g]<mucchioBin[f+1] && (f+1)<(v.size()-k-1))
+    {
+         bicerino = mucchioBin[f+1];
+         mucchioBin[f+1] = mucchioBin[g];
+         mucchioBin[g] = bicerino;
+         g=f+1;
+    }
+    //Se ambo le condizioni non sono verificate il nodo ha un etichetta maggiore di ambo i suoi [eventuali] nodi figli, condizione che si verifica solo se l'algoritmo è terminato.
+    else g=v.size();
 
   }
-
 }
+*/
 
-#endif // __ORDINAMENTO_H
